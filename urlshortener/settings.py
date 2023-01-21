c0b7630d341afb87283d25b663b0ac52b2c73a55
M Sharjeel Shaikh <119777212+M-Sharjeel-Shaikh@users.noreply.github.com>
2023-01-21 18:03:49 +0500

    Update setting.py
    Removing Credential Information

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -98,5 +98,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shaikhsharjeel02@gmail.com'
-EMAIL_HOST_PASSWORD = 'lhgqfqduacqngtrr'
+EMAIL_HOST_USER = 'your email here '
+EMAIL_HOST_PASSWORD = 'app secret id'
